be4b65209cb435922089d8c1f25aab82916de6d3
New User <newuser@example.com>
2024-05-09 19:13:27 +0300

    Added deck generation

--- war-game.py
+++ war-game.py
@@ -1,5 +1,26 @@
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+    
+    def __repr__(self):
+        return f"{self.suit} {self.value}"
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["hearts", "diamonds", "spades", "clubs"] 
+        for suit in suits:
+            for value in range(1, 15):
+                card = Card(suit, value)
+                self.cards.append(card)
+    
+    def __str__(self):
+        return f"{self.cards}"
+
 def main():
-    pass
+    deck = Deck()
+    
 
 if __name__ == "__main__":
     main()
