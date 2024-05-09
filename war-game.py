9cffc7a9946b10053cf2433283aa80f87d1a933c
New User <newuser@example.com>
2024-05-09 19:23:42 +0300

    Deck splitting

--- war-game.py
+++ war-game.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
     def __init__(self, suit, value):
         self.suit = suit
@@ -15,11 +17,38 @@ class Deck:
                 card = Card(suit, value)
                 self.cards.append(card)
     
+    def shuffle_deck(self):
+        random.shuffle(self.cards)
+
+    def split_deck(self):
+        print(self.cards)
+        cards_for_one_player = len(self.cards) // 2
+        deck1 = self.cards[:cards_for_one_player]
+        deck2 = self.cards[cards_for_one_player:]
+        return deck1, deck2
+
     def __str__(self):
         return f"{self.cards}"
 
+class Player:
+    def __init__(self):
+        self.deck = []
+        has_lost = False
+
+class Game:
+    def __init__(self, deck):
+        self.player1 = Player()
+        self.player2 = Player()
+        self.player1.deck, self.player2.deck = deck.split_deck()
+        print(self.player1.deck, self.player2.deck)
+
 def main():
     deck = Deck()
+    deck.shuffle_deck()
+    game = Game(deck)
+
+    
+
     
 
 if __name__ == "__main__":
