02ad0931fbc0f8122669941f1be364f4d471160e
New User <newuser@example.com>
2024-05-10 18:40:25 +0300

    reconstruction

--- war-game.py
+++ war-game.py
@@ -1,15 +1,31 @@
-import random
+import random, os
 
 class Card:
     def __init__(self, suit, value):
         self.suit = suit
-        self.value = value
+        self._value = value
+        type = ""
+        if value == 14:
+            type = "Ace"
+        elif value == 13:
+            type = "King"
+        elif value == 12:
+            type = "Queen"
+        elif value == 11:
+            type = "Jack"
+        else:
+            type = str(self._value)
+        self.name = type + " of " + self.suit
     
-    def get_value(self):
-        return self.value
+    @property
+    def value(self):
+        return self._value
     
     def __repr__(self):
-        return f"{self.suit} {self.value}"
+        return f"{self.suit} {self._value}"
+    
+    def __str__(self):
+        return f"{self.name}"
 
 class Deck:
     def __init__(self):
@@ -29,7 +45,6 @@ class Deck:
         self.deck2 = self.cards[cards_for_one_player:]
         return self.deck1, self.deck2
 
-
     def __str__(self):
         return f"{self.cards}"
 
@@ -47,21 +62,30 @@ class Game:
 
     def fight(self):
         while not self.player1.has_lost and not self.player2.has_lost:
-            card1 = self.player1.deck[0].get_value()
-            card2 = self.player2.deck[0].get_value()
+            card1 = self.player1.deck[0].value
+            card2 = self.player2.deck[0].value
             if card1 > card2:
                 self.player1.deck.append(card2)
-                self.player2.deck.pop([0])
+                self.player2.deck.pop(0)
+
+    def show_deck(self):
+        clear()
+        print("Your deck is: ")
+        for card in self.player2.deck:
+            print(card)
+            
 
 def main():
     deck = Deck()
     deck.shuffle_deck()
     game = Game(deck)
     if input("Start the game? "):
+        game.show_deck()
         game.fight()
-    
 
-    
+def clear():
+    os.system('clear' if os.name=='nt' else 'clear')
+
 
 if __name__ == "__main__":
     main()
